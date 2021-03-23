0d533a841e5f6ef5f15c287baa49965a41edf06a
Gavin Purcell <mkorangestripe@gmail.com>
2021-03-22 20:45:50 -0500

    Add message about load balancer to content

--- fake_vms.py
+++ fake_vms.py
@@ -1,20 +1,26 @@
 class FakeVMs:
     """
     These methods emulate virtual machines that the load balancer distrubutes traffic to.
-    The bigger cats can take more traffic.
+    The bigger cats take more traffic.
     """
 
     def __init__(self):
+        self.content_list = ["Load balancer simulator. The bigger cats take more traffic.<br/><br/>\n", ""]
         self.content = ""
+        self.listToStr = ""
 
     def vm1(self):
-        self.content = "cheetah"
+        self.content_list[1] = "cheetah"
+        self.content = self.listToStr.join(self.content_list)
 
     def vm2(self):
-        self.content = "ocelot"
+        self.content_list[1] = "ocelot"
+        self.content = self.listToStr.join(self.content_list)
 
     def vm3(self):
-        self.content = "mountain lion"
+        self.content_list[1] = "mountain lion"
+        self.content = self.listToStr.join(self.content_list)
 
     def vm4(self):
-        self.content = "jaguarundi"
+        self.content_list[1] = "jaguarundi"
+        self.content = self.listToStr.join(self.content_list)
