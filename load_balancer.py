4f5d962323f1fbe3acbd4f7a49f752aa63ade96c
Gavin Purcell <mkorangestripe@gmail.com>
2022-11-20 14:01:16 -0600

    Add github workflow and .pylint, delete fake_vms.py

--- load_balancer.py
+++ load_balancer.py
@@ -5,8 +5,8 @@ This load balancer distributes requests between multiple Docker containers.
 """
 
 import os
-import requests
 import sys
+import requests
 import yaml
 from flask import Flask
 
@@ -15,6 +15,7 @@ class LoadBalancerMethod:
     """Load balancer methods"""
 
     def __init__(self, vms):
+        self.vms = vms
         self.vm_hostnames = list(vms.keys())
         self.vm_count = len(self.vm_hostnames)
         self.ratio_i = 1
@@ -28,10 +29,10 @@ class LoadBalancerMethod:
             self.ratio -= 1
         elif self.target_vm_i == self.vm_count - 1:
             self.target_vm_i = 0
-            self.ratio = vms[self.vm_hostnames[self.target_vm_i]][self.ratio_i]
+            self.ratio = self.vms[self.vm_hostnames[self.target_vm_i]][self.ratio_i]
         else:
             self.target_vm_i += 1
-            self.ratio = vms[self.vm_hostnames[self.target_vm_i]][self.ratio_i]
+            self.ratio = self.vms[self.vm_hostnames[self.target_vm_i]][self.ratio_i]
         return vm
 
 
@@ -41,12 +42,12 @@ if os.path.exists(host_list_file) is False:
     print("\n" + str(host_list_file), "not found")
     sys.exit(1)
 
-with open(host_list_file, 'r') as f:
+with open(host_list_file, 'r', encoding="utf-8") as f:
     vms_yaml = f.read()
 
-vms = yaml.safe_load(vms_yaml)
+vms_dict = yaml.safe_load(vms_yaml)
 
-load_balancer_method = LoadBalancerMethod(vms)
+load_balancer_method = LoadBalancerMethod(vms_dict)
 
 app = Flask(__name__)
 
@@ -54,5 +55,5 @@ app = Flask(__name__)
 def distrubute():
     """Distribute the load"""
     vm = load_balancer_method.round_robin_selector()
-    r = requests.get('http://' + vm)
+    r = requests.get('http://' + vm, timeout=5)
     return str(r.text)
